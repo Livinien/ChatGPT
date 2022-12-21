7b85b3a6e4907e44f430ac996a9dbd96eab84d0c
Flavien <livinien@hotmail.fr>
2022-12-21 18:25:09 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+// APPARITION DU MODAL AUTOMATIQUE //
+
+const myModal = document.querySelector('#animation_modal')
+
+myModal.addEventListener('show.bs.modal', event => {
+  if (!data) {
+    return event.preventDefault() // stops modal from being shown
+  }
+})
