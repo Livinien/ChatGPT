203dac22833c5a427b1fb33f41bf4991800cc043
Flavien <livinien@hotmail.fr>
2022-12-23 10:37:31 +0100

    Ajout de la barre de recherche + le subtitle en dessous du la barre avec du css en plus

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 // APPARITION DU MODAL AUTOMATIQUE AU RAFRAICHISEMMENT DE LA PAGE //
 
-$(window).on('load', function() {
-  $('#Modal_Toggle_1').modal('show');
-});
+// $(window).on('load', function() {
+//   $('#Modal_Toggle_1').modal('show');
+// });
 
